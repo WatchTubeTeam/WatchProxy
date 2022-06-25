c60697816829267de15e1fb63036fb52d385207c
BBaoVanC <bbaovanc@bbaovanc.com>
2022-06-24 22:39:44 -0500

    a

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use actix_web::{
+    web,
     HttpServer,
     HttpResponse,
     Responder,
     App,
     get,
 };
+use hyper::{
+    client::HttpConnector,
+    Body,
+    Uri,
+};
+
+type Client = hyper::client::Client<HttpConnector, Body>;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .app_data(Client::new().clone())
             .service(root)
     })
     .bind(("127.0.0.1", 8080))?
@@ -20,4 +29,17 @@ async fn main() -> std::io::Result<()> {
 #[get("/")]
 async fn root() -> impl Responder {
     HttpResponse::Ok().body("Hello world")
+}
+
+#[get("/test")]
+async fn test(client: web::Data<Client>) -> Result<hyper::Response<Body>, hyper::Error> {
+    let resp = client.get(Uri::from_static("https://manifest.watchtube.app")).await;
+
+    resp
+    /*
+    match resp {
+        Ok(r) => HttpResponse::Ok().body(r),
+        Err(e) => HttpResponse::BadGateway().body(e.to_string()),
+    }
+    */
 }
