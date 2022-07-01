abc778c2ed4113b1314f4e57378ac536684fb74a
BBaoVanC <bbaovanc@bbaovanc.com>
2022-06-30 20:58:03 -0500

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
-use axum::{extract::Extension, response::IntoResponse, routing, Router};
+use axum::{extract::Extension, response::{IntoResponse, Response}, routing, Router};
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+
 use http::StatusCode;
-use hyper::{client::HttpConnector, Body, Client, Uri};
-use hyper_tls::HttpsConnector;
 use std::net::SocketAddr;
 
+use anyhow::Context;
+use thiserror::Error;
+
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
-    let https = HttpsConnector::new();
-    let client = Client::builder().build::<_, Body>(https);
+    let http_client = reqwest::Client::new();
 
     let app = Router::new()
         .route("/", routing::get(root))
-        .layer(Extension(client));
+        .layer(
+            ServiceBuilder::new()
+                .layer(TraceLayer::new_for_http())
+                .layer(Extension(http_client))
+        );
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     tracing::info!("Listening on {addr}");
@@ -21,20 +28,27 @@ async fn main() {
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
-        .unwrap();
+        .context("Error starting axum server")
 }
 
-async fn root(
-    Extension(client): Extension<Client<HttpsConnector<HttpConnector>>>,
-) -> Result<hyper::Response<Body>, (StatusCode, &'static str)> {
-    match client
-        .get(Uri::from_static("https://manifest.watchtube.app"))
-        .await
-    {
-        Ok(resp) => Ok(resp),
-        Err(err) => {
-            tracing::warn!("Error fetching WatchTube manifest: {:?}", err);
-            Err((StatusCode::INTERNAL_SERVER_ERROR, "Error fetching manifest"))
+
+#[derive(Error, Debug)]
+enum ProxyError {
+    #[error("Outbound request failed")]
+    OutboundRequestFailure(#[from] reqwest::Error),
+}
+
+impl IntoResponse for ProxyError {
+    fn into_response(self) -> Response {
+        match self {
+            ProxyError::OutboundRequestFailure(_err) => (StatusCode::BAD_GATEWAY, "hello").into_response()
         }
     }
+}
+
+
+async fn root(
+    Extension(client): Extension<reqwest::Client>,
+) -> Result<String, ProxyError> {
+    Ok(client.get("https://manifest.watchtube.app").send().await?.text().await?)
 }
